80153ae5a27db0cf833136e82918bef6a366578c
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-04-18 21:37:06 -0400

    Removing CSV loader code

--- load_games_from_csv.py
+++ load_games_from_csv.py
@@ -27,20 +27,4 @@ if __name__ == '__main__':
     # agent_executor = create_sql_agent(ChatOpenAI(), db=db, agent_type="openai-tools", verbose=True)
     # print(agent_executor.invoke({"input": "what games were played on october 18th 2022?"})['output'])
 
-    # loader = CSVLoader(file_path='./assets/2022-2023_season.csv', csv_args={
-    #     'delimiter': ',',
-    #     'fieldnames': ['Date', 'Start (ET)', 'Visitor team', 'Visitor team\'s points', 'Home team',
-    #                    'Home team\'s points', 'Attendance', 'Arena', 'Notes']
-    # })
-    # data = loader.load()
-    #
-    # print("Splitting retrieved information")
-    # text_splitter = RecursiveCharacterTextSplitter(chunk_size=200000, chunk_overlap=200)
-    # splits = text_splitter.split_documents(data)
-    #
-    # print("Storing information in a vector store (this might take a while)")
-    # vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(),
-    #                                     persist_directory="./vectorstores/2023season")
-    #
-    # print("Storage complete")
 
