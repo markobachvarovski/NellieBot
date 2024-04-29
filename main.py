be7f1e1ebf43728a750d4e8bfb0b1c3aaa198556
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-04-28 21:56:55 -0400

    Added memorization ability

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.chains.sql_database.query import create_sql_query_chain
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_community.utilities.sql_database import SQLDatabase
+from langchain_core.runnables import RunnablePassthrough
+from langchain_experimental.sql import SQLDatabaseChain
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings, OpenAI
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
@@ -30,80 +32,60 @@ if __name__ == '__main__':
     load_dotenv()
     print("Nellie: Hi! I'm Nellie, your personal NBA assistant. Hang tight while I load some information for you\n")
 
-    # print("Fetching information")
-    # vectorstore = Chroma(persist_directory="./vectorstores/2023season", embedding_function=OpenAIEmbeddings())
-    # retriever = vectorstore.as_retriever()
-
     chat = ChatOpenAI()
+    output_parser = StrOutputParser()
     db = SQLDatabase.from_uri("sqlite:///games.db")
 
-    sql_template = '''Given an input question, first create a syntactically correct sqlite query to run, then look at 
+    sql_template = '''Given a question, first create a syntactically correct sqlite query to run, then look at 
     the results of the query and return the answer. Select up to {top_k} rows from the following tables: {table_info}
-    Retrieve only the output from the database. Use the following format:
+    Retrieve only the output from the database. Use the following format to process the query:
 
-    Question: "{input}"
-    SQL Query: "query"
-    SQL Result: "result"
+    Question: "Question"
+    SQLQuery: "SQL Query to run"
+    SQLResult: "Result of the SQLQuery"
+    Answer: "Final answer here"
+    
+    Only return the answer, without the SQL result and SQL query.
     
     If given a date, format it in the format "MMMM DD, YYYY". Example: november 1st 2022 would be "November 1, 
     2022". Search for dates in this format. If asked for games on a specific date, list them all, not just the first 
     one.
+    
+    Question: {input}
     '''
-    sql_prompt = PromptTemplate.from_template(sql_template
-        # [
-        #     ("system", sql_template),
-        #     MessagesPlaceholder("messages"),
-        #     ("human", "{input}"),
-        # ]
-    )
-    # sql_chain = sql_prompt | chat
+    sql_prompt = PromptTemplate.from_template(sql_template)
     sql_chain = create_sql_query_chain(chat, db, prompt=sql_prompt)
+    # sql_chain = SQLDatabaseChain.from_llm(chat, db, prompt=sql_prompt, return_intermediate_steps=False)
     # sql_chain = create_stuff_documents_chain(chat, sql_prompt)
     # sql_prompt = PromptTemplate.from_template(sql_template)
 
-    # print("Creating prompt to contextualize question")
-    # add_context_to_question_prompt = ChatPromptTemplate.from_messages(
-    #     [
-    #         ("system", """Given a chat history and the latest user question which might reference context in the chat
-    #         history, formulate a standalone question which can be understood without the chat history. Do NOT answer the
-    #         question, just reformulate it if needed and otherwise return it as is. The context consists of information
-    #         about NBA games played"""),
-    #         MessagesPlaceholder("messages"),
-    #         ("human", "{input}"),
-    #     ]
-    # )
-    # add_context_to_question_retriever = create_history_aware_retriever(
-    #     chat, retriever, add_context_to_question_prompt
-    # )
+    add_context_to_question_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", """Given a chat history and the latest user question which might reference context in the chat 
+            history, formulate a standalone question which can be understood without the chat history. Do NOT answer 
+            the question, just reformulate it if needed and otherwise return it as is. The context consists of 
+            information about NBA games played. Keep question concide, use the same wording for every question when 
+            adding context. When a general question is asked about who played a game, assume the user is asking for 
+            all teams playing that day, unless specified otherwise. Mention explicitly when the question asks for a 
+            specific team versus all teams"""),
+            MessagesPlaceholder("messages"),
+            ("human", "{input}"),
+        ]
+    )
 
-    # print("Creating prompt to answer questions")
-    # answer_question_prompt = ChatPromptTemplate.from_messages(
-    #     [
-    #         (
-    #             "system",
-    #             "Your name is Nellie. You are a chatbot having a conversation with a human about the NBA. You should "
-    #             "answer questions about the NBA players, past or present, their career statistics and accolades. In "
-    #             "addition to your trained data, use the following pieces of retrieved context to answer the question. "
-    #             "If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the "
-    #             "answer concise."
-    #             ""
-    #             "{context}",
-    #         ),
-    #         MessagesPlaceholder(variable_name="messages"),
-    #         ("human", "{input}")
-    #     ]
-    # )
-    # answer_question_retriever = create_stuff_documents_chain(chat, answer_question_prompt)
-    #
-    # chain = create_retrieval_chain(add_context_to_question_retriever, answer_question_retriever)
+    context_chain = add_context_to_question_prompt | chat | {"input": output_parser,
+                                                             "question": output_parser,
+                                                             "top_k": RunnablePassthrough(),
+                                                             "table_info": RunnablePassthrough()}
 
-    print("Chaining retrievers together\n")
+    chain = context_chain | sql_chain
+
     conversation = RunnableWithMessageHistory(
-        sql_chain,
+        chain,
         get_session_history,
         input_messages_key="input",
         history_messages_key="messages",
-        output_messages_key="answer",
+        # output_messages_key="answer",
     )
 
     print("Nellie: I'm ready! Ask me anything or enter ':q' to quit")
@@ -113,14 +95,10 @@ if __name__ == '__main__':
             print("Nellie: See you soon!\n\nExited successfully")
             break
         else:
-            # res = conversation.invoke(
-            #     {"input": userMessage},
-            #     {"configurable":
-            #          {"session_id": "4"}
-            #      },
-            # )
-            # print(res)
-
-            res = sql_chain.invoke(
-                {"input": userMessage, "question": userMessage, "top_k": "20", "table_info": "games"})
+            res = conversation.invoke(
+                {"input": userMessage, "top_k": "20", "table_info": "games"},
+                {"configurable":
+                     {"session_id": "1"}
+                 },
+            )
             print(res)
